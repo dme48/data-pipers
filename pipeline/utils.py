c9c1d9b0c6c8d1963eee5ed1aa6351e095f07db9
dme48 <dmartinezechevarria@gmail.com>
2022-02-12 12:50:48 +0100

    Using -r flag to read token

--- pipeline/utils.py
+++ pipeline/utils.py
@@ -2,6 +2,6 @@
 
 def read_token(filename: str = "token"):
     """Reads the file (filename) and returns it as a string"""
-    with open(filename) as token_file:
+    with open(filename, "r") as token_file:
         token = token_file.read()
     return token.replace("\n", "")

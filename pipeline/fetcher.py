37bc0c8a0440824905b6c17d3a73ad6174d71528
dme48 <dmartinezechevarria@gmail.com>
2022-02-13 18:28:31 +0100

    Finished extractor function

--- pipeline/fetcher.py
+++ pipeline/fetcher.py
@@ -1,6 +1,6 @@
 import json
 from typeform import Typeform
-from utils import read_question_ids, read_token
+from utils import read_question_ids, read_token, extract_answers
 
 ids_to_questions = read_question_ids()
 token = read_token()
@@ -8,9 +8,8 @@ token = read_token()
 responses = Typeform(token).responses
 result: dict = responses.list('K43EfCnQ')
 
+bare_answers = {i: [] for i in range(len(ids_to_questions))}
 
-with open('results.json', 'w+') as f:
-    json.dump(result, f, indent=4)
-
-for key in result.keys():
-    print(key)
+for response in result["items"]:
+    answers = extract_answers(response)
+    print(answers)
--- pipeline/utils.py
+++ pipeline/utils.py
@@ -1,6 +1,7 @@
 """Collection of utility functions for the pipeline"""
 import json
 
+
 def read_question_ids(filename: str = "question_ids.json"):
     """Reads the file (filename) and returns it as a dictionary"""
     with open(filename, "r") as json_file:
@@ -13,3 +14,37 @@ def read_token(filename: str = "token"):
     with open(filename, "r") as token_file:
         token = token_file.read()
     return token.replace("\n", "")
+
+
+def extract_answers(response: dict):
+    """
+    Extracts the answers from a Typeform formatted dict (single solved form)
+    Arguments:
+        response (dict): Typeform's dict for a single solved form
+    Returns:
+        list containing the (formatted) answers to the form
+    """
+    final_answers = []
+    for question in response["answers"]:
+        final_answers.append(format_answer(question))
+    return final_answers
+
+
+FORMAT_GUIDE = {
+    "text": lambda x: x,
+    "boolean": lambda x: x,
+    "number": lambda x: float(x),
+    "choice": lambda x: x["label"],
+    "choices": lambda x: x["labels"]
+}
+
+
+def format_answer(question):
+    """
+    Extracts the answer from a question and formats it. Accordingly
+    to the FORMAT_GUIDE.
+    """
+    answer_type = question["type"]
+    answer = question[answer_type]
+
+    return FORMAT_GUIDE[answer_type](answer)

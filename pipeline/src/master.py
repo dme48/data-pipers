c34ae674d0a409233c281384f6a92ab0681360c4
dme48 <dmartinezechevarria@gmail.com>
2022-02-24 09:19:17 +0100

    Implemented init and update methods

--- pipeline/src/master.py
+++ pipeline/src/master.py
@@ -12,7 +12,7 @@ class MasterData:
     providing the ability to update and return them separately. 
     """
 
-    def __init__(self, config: dict):
+    def __init__(self, config: dict = None):
         """
         Initializes the MasterData instance by fetching the data from the
         different sources.
@@ -21,12 +21,35 @@ class MasterData:
         fetch_typeform and fetch_monkey, stored at the keys "typeform_login",
         "typeform_config", "monkey_login" and "monkey_config". 
         """
+        self.config = config
+        self.typeform_data = None
+        self.monkey_data = None
+        self.update_all()
 
     def update_typeform(self) -> None:
         """Updates the data corresponding to the Typeform surveys"""
+        if not config:
+            self.typeform_data = fetch_typeform()
+            return
+
+        login = self.config["typeform_login"]
+        config = self.config["typeform_config"]
+        self.typeform_data = fetch_typeform(login, config)
 
     def update_monkey(self) -> None:
         """Updates the data corresponding to the Survey Monkey surveys"""
+        if not config:
+            self.monkey_data = fetch_monkey()
+            return
+
+        login = self.config["monkey_login"]
+        config = self.config["monkey_config"]
+        self.monkey_data = fetch_monkey(login, config)
+
+    def update_all(self) -> None:
+        """Updates the data from every form"""
+        self.update_typeform()
+        self.update_monkey()
 
     def get_typeform_data(self) -> pd.DataFrame:
         """Returns the data corresponding to Typeform as a panads DF"""
